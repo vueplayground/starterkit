b0bb17d6695a042e23c3c681914abdd9566e170c
Vue Play <author@vueplay.com>
2024-02-13 09:55:21 +0100

    Stored by Vue Play

--- src/components/templates/Home.vue
+++ src/components/templates/Home.vue
@@ -5,7 +5,7 @@
 				<PageContainer class="h-full">
 					<div class="flex-row w-1/2 h-full flex"><input
 							style="height:25px;margin-top:7px;margin-left:15px"
-							class="rounded min-w-fit w-4 bg-sky-50"
+							class="w-32 rounded md:min-w-fit bg-sky-50"
 						/><button class="hover:bg-indigo-900 ml-2 px-2 h-full"><svg
 								xmlns="http://www.w3.org/2000/svg"
 								viewBox="0 0 24 24"
@@ -17,8 +17,7 @@
 									d="M10.5 3.75a6.75 6.75 0 100 13.5 6.75 6.75 0 000-13.5zM2.25 10.5a8.25 8.25 0 1114.59 5.28l4.69 4.69a.75.75 0 11-1.06 1.06l-4.69-4.69A8.25 8.25 0 012.25 10.5z"
 									clip-rule="evenodd"
 								/>
-							</svg> </button>
-					</div>
+							</svg> </button> </div>
 					<div class="flex-row-reverse w-1/2 h-full flex">
 						<button class="hover:bg-indigo-900 px-2 h-full"><svg
 								xmlns="http://www.w3.org/2000/svg"
@@ -42,8 +41,7 @@
 									d="M15.75 1.5a6.75 6.75 0 00-6.651 7.906c.067.39-.032.717-.221.906l-6.5 6.499a3 3 0 00-.878 2.121v2.818c0 .414.336.75.75.75H6a.75.75 0 00.75-.75v-1.5h1.5A.75.75 0 009 19.5V18h1.5a.75.75 0 00.53-.22l2.658-2.658c.19-.189.517-.288.906-.22A6.75 6.75 0 1015.75 1.5zm0 3a.75.75 0 000 1.5A2.25 2.25 0 0118 8.25a.75.75 0 001.5 0 3.75 3.75 0 00-3.75-3.75z"
 									clip-rule="evenodd"
 								/>
-							</svg>
-						</button>
+							</svg> </button>
 					</div>
 				</PageContainer>
 			</div>
