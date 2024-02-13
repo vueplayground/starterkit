84092a0b3bd7167e4f1cb16a6d53a21c93839549
Vue Play <author@vueplay.com>
2024-02-13 11:13:50 +0100

    Stored by Vue Play

--- src/components/organisms/PageContainer.vue
+++ src/components/organisms/PageContainer.vue
@@ -1,6 +1,10 @@
 <template>
 	<div class="flex m-auto max-w-6xl w-full">
-		<slot></slot>
+		<slot>
+			<div class="p-4">
+				Add anything here
+			</div>
+		</slot>
 	</div>
 </template>
 <script>
