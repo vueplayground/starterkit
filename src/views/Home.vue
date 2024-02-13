fc0ca89361409ac007ae6b67d3b1f73deb39c2e7
Vue Play <author@vueplay.com>
2024-02-13 16:56:23 +0100

    Stored by Vue Play

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
 	<Hero
 		class="shadow-2xl"
-		title="Vue Play -  Starte"
+		title="Vue Play -  Starter Kitw"
 	>
 		<PageContainer>
 			<div class="w-full md:flex-row flex-col my-16 py-4 flex">
