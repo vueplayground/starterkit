34b360b8c4d61111daeea0e5af909742e91606da
Vue Play <author@vueplay.com>
2024-02-13 11:43:10 +0100

    Stored by Vue Play

---
+++ src/views/Video.vue
@@ -0,0 +1,28 @@
+<template>
+	<Default>
+		<PageContainer class="flex-col">
+			<h1 class="mb-2 mt-12 font-medium text-3xl">
+				Video
+			</h1>
+			<div>
+				<div>
+				</div>
+				<div>
+				</div>
+			</div>
+		</PageContainer>
+	</Default>
+</template>
+<script>
+	import PageContainer from '@/components/organisms/PageContainer.vue';
+	import Default from '@/components/templates/Default.vue';
+	export default {
+		components: {
+			PageContainer: PageContainer,
+			Default: Default
+		},
+		data: () => ({})
+	};
+
+</script>
+<style scoped></style>
