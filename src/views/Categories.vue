c2dfb03ef2c3c0cc0101184ea85c423c80e39169
Vue Play <author@vueplay.com>
2024-02-13 16:43:43 +0100

    Stored by Vue Play

--- src/views/Categories.vue
+++ src/views/Categories.vue
@@ -4,21 +4,114 @@
 
 			<div class="flex-col max-w-md place-self-center mt-8 mb-24 w-full flex md:max-w-2xl">
 				<h1 class="px-4 mb-4 mt-6 font-medium text-3xl w-full">
-					Terms of Service
-				</h1> <span class="text-slate-500 pl-5 mt-2">
-					Dec 22, 2023
-				</span>
-				<div class="relative mb-16 flex-col md:flex-row flex w-full px-4">
-					<div class="pt-2 md:pt-0 grow">
-						<h2 class="text-xl font-medium mt-10">
-							Lorem ipsum dolor sit amet
-						</h2> <span class="text-slate-800 my-4 w-full block">
-							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla facilisi. Sed ut justo quis nisi placerat faucibus. Integer feugiat eros at nisi commodo, sed dictum eros volutpat.
-						</span><span class="text-slate-800 my-4 w-full block">
-							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla facilisi. Sed ut justo quis nisi placerat faucibus. Integer feugiat eros at nisi commodo, sed dictum eros volutpat.
-						</span>
+					Categories
+				</h1>
+				<div class="mt-6 relative mb-16 flex-col md:flex-row flex w-full px-4">
+					<div class="gap-11 grid grid-cols-3 w-full">
+						<div class="shadow-md rounded-lg items-center justify-center flex-col bg-gradient-to-tr from-slate-500 bg-slate-400 h-28 flex cursor-pointer hover:opacity-80">
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								viewBox="0 0 24 24"
+								fill="currentColor"
+								class="text-white w-12 h-12"
+							>
+								<path
+									fill-rule="evenodd"
+									d="M8.161 2.58a1.875 1.875 0 011.678 0l4.993 2.498c.106.052.23.052.336 0l3.869-1.935A1.875 1.875 0 0121.75 4.82v12.485c0 .71-.401 1.36-1.037 1.677l-4.875 2.437a1.875 1.875 0 01-1.676 0l-4.994-2.497a.375.375 0 00-.336 0l-3.868 1.935A1.875 1.875 0 012.25 19.18V6.695c0-.71.401-1.36 1.036-1.677l4.875-2.437zM9 6a.75.75 0 01.75.75V15a.75.75 0 01-1.5 0V6.75A.75.75 0 019 6zm6.75 3a.75.75 0 00-1.5 0v8.25a.75.75 0 001.5 0V9z"
+									clip-rule="evenodd"
+								/>
+							</svg>
+							<h2 class="text-slate-100">
+								Category 1
+							</h2>
+						</div>
+						<div class="shadow-md rounded-lg items-center justify-center flex-col bg-gradient-to-tr from-slate-500 bg-slate-400 h-28 flex cursor-pointer hover:opacity-80">
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								viewBox="0 0 24 24"
+								fill="currentColor"
+								class="text-white w-12 h-12"
+							>
+								<path
+									fill-rule="evenodd"
+									d="M8.161 2.58a1.875 1.875 0 011.678 0l4.993 2.498c.106.052.23.052.336 0l3.869-1.935A1.875 1.875 0 0121.75 4.82v12.485c0 .71-.401 1.36-1.037 1.677l-4.875 2.437a1.875 1.875 0 01-1.676 0l-4.994-2.497a.375.375 0 00-.336 0l-3.868 1.935A1.875 1.875 0 012.25 19.18V6.695c0-.71.401-1.36 1.036-1.677l4.875-2.437zM9 6a.75.75 0 01.75.75V15a.75.75 0 01-1.5 0V6.75A.75.75 0 019 6zm6.75 3a.75.75 0 00-1.5 0v8.25a.75.75 0 001.5 0V9z"
+									clip-rule="evenodd"
+								/>
+							</svg>
+							<h2 class="text-slate-100">
+								Category 2
+							</h2>
+						</div>
+						<div class="shadow-md rounded-lg items-center justify-center flex-col bg-gradient-to-tr from-slate-500 bg-slate-400 h-28 flex cursor-pointer hover:opacity-80">
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								viewBox="0 0 24 24"
+								fill="currentColor"
+								class="text-white w-12 h-12"
+							>
+								<path
+									fill-rule="evenodd"
+									d="M8.161 2.58a1.875 1.875 0 011.678 0l4.993 2.498c.106.052.23.052.336 0l3.869-1.935A1.875 1.875 0 0121.75 4.82v12.485c0 .71-.401 1.36-1.037 1.677l-4.875 2.437a1.875 1.875 0 01-1.676 0l-4.994-2.497a.375.375 0 00-.336 0l-3.868 1.935A1.875 1.875 0 012.25 19.18V6.695c0-.71.401-1.36 1.036-1.677l4.875-2.437zM9 6a.75.75 0 01.75.75V15a.75.75 0 01-1.5 0V6.75A.75.75 0 019 6zm6.75 3a.75.75 0 00-1.5 0v8.25a.75.75 0 001.5 0V9z"
+									clip-rule="evenodd"
+								/>
+							</svg>
+							<h2 class="text-slate-100">
+								Category 3
+							</h2>
+						</div>
+						<div class="shadow-md rounded-lg items-center justify-center flex-col bg-gradient-to-tr from-slate-500 bg-slate-400 h-28 flex cursor-pointer hover:opacity-80">
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								viewBox="0 0 24 24"
+								fill="currentColor"
+								class="text-white w-12 h-12"
+							>
+								<path
+									fill-rule="evenodd"
+									d="M8.161 2.58a1.875 1.875 0 011.678 0l4.993 2.498c.106.052.23.052.336 0l3.869-1.935A1.875 1.875 0 0121.75 4.82v12.485c0 .71-.401 1.36-1.037 1.677l-4.875 2.437a1.875 1.875 0 01-1.676 0l-4.994-2.497a.375.375 0 00-.336 0l-3.868 1.935A1.875 1.875 0 012.25 19.18V6.695c0-.71.401-1.36 1.036-1.677l4.875-2.437zM9 6a.75.75 0 01.75.75V15a.75.75 0 01-1.5 0V6.75A.75.75 0 019 6zm6.75 3a.75.75 0 00-1.5 0v8.25a.75.75 0 001.5 0V9z"
+									clip-rule="evenodd"
+								/>
+							</svg>
+							<h2 class="text-slate-100">
+								Category 4
+							</h2>
+						</div>
+						<div class="shadow-md rounded-lg items-center justify-center flex-col bg-gradient-to-tr from-slate-500 bg-slate-400 h-28 flex cursor-pointer hover:opacity-80">
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								viewBox="0 0 24 24"
+								fill="currentColor"
+								class="text-white w-12 h-12"
+							>
+								<path
+									fill-rule="evenodd"
+									d="M8.161 2.58a1.875 1.875 0 011.678 0l4.993 2.498c.106.052.23.052.336 0l3.869-1.935A1.875 1.875 0 0121.75 4.82v12.485c0 .71-.401 1.36-1.037 1.677l-4.875 2.437a1.875 1.875 0 01-1.676 0l-4.994-2.497a.375.375 0 00-.336 0l-3.868 1.935A1.875 1.875 0 012.25 19.18V6.695c0-.71.401-1.36 1.036-1.677l4.875-2.437zM9 6a.75.75 0 01.75.75V15a.75.75 0 01-1.5 0V6.75A.75.75 0 019 6zm6.75 3a.75.75 0 00-1.5 0v8.25a.75.75 0 001.5 0V9z"
+									clip-rule="evenodd"
+								/>
+							</svg>
+							<h2 class="text-slate-100">
+								Category 5
+							</h2>
+						</div>
+						<div class="shadow-md rounded-lg items-center justify-center flex-col bg-gradient-to-tr from-slate-500 bg-slate-400 h-28 flex cursor-pointer hover:opacity-80">
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								viewBox="0 0 24 24"
+								fill="currentColor"
+								class="text-white w-12 h-12"
+							>
+								<path
+									fill-rule="evenodd"
+									d="M8.161 2.58a1.875 1.875 0 011.678 0l4.993 2.498c.106.052.23.052.336 0l3.869-1.935A1.875 1.875 0 0121.75 4.82v12.485c0 .71-.401 1.36-1.037 1.677l-4.875 2.437a1.875 1.875 0 01-1.676 0l-4.994-2.497a.375.375 0 00-.336 0l-3.868 1.935A1.875 1.875 0 012.25 19.18V6.695c0-.71.401-1.36 1.036-1.677l4.875-2.437zM9 6a.75.75 0 01.75.75V15a.75.75 0 01-1.5 0V6.75A.75.75 0 019 6zm6.75 3a.75.75 0 00-1.5 0v8.25a.75.75 0 001.5 0V9z"
+									clip-rule="evenodd"
+								/>
+							</svg>
+							<h2 class="text-slate-100">
+								Category 6
+							</h2>
+						</div>
 					</div>
-					<div class="bg-slate-100 -bottom-5 absolute h-0.5 left-4 right-4">
+					<div class="-mb-8 bg-slate-100 -bottom-5 absolute h-0.5 left-4 right-4">
 					</div>
 				</div>
 			</div>
