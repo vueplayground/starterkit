da93f3f260503d64515aaaf4a1a2121a35d8e690
Vue Play <author@vueplay.com>
2024-02-13 10:12:35 +0100

    Stored by Vue Play

--- src/components/templates/Home.vue
+++ src/components/templates/Home.vue
@@ -6,7 +6,7 @@
 					<div class="flex-row w-1/2 h-full flex"><input
 							style="height:25px;margin-top:7px;margin-left:15px"
 							class="w-32 px-2 rounded md:min-w-fit bg-sky-50"
-						/><button class="hover:bg-indigo-900 ml-2 px-2 h-full"><svg
+						/><button class="hover:bg-blue-900 ml-2 px-2 h-full"><svg
 								xmlns="http://www.w3.org/2000/svg"
 								viewBox="0 0 24 24"
 								fill="currentColor"
@@ -19,7 +19,7 @@
 								/>
 							</svg> </button> </div>
 					<div class="flex-row-reverse w-1/2 h-full flex">
-						<button class="hover:bg-indigo-900 px-2 h-full"><svg
+						<button class="hover:bg-blue-900 px-2 h-full"><svg
 								xmlns="http://www.w3.org/2000/svg"
 								viewBox="0 0 24 24"
 								fill="currentColor"
@@ -30,7 +30,7 @@
 									d="M4.5 3.75a3 3 0 00-3 3v10.5a3 3 0 003 3h15a3 3 0 003-3V6.75a3 3 0 00-3-3h-15zm4.125 3a2.25 2.25 0 100 4.5 2.25 2.25 0 000-4.5zm-3.873 8.703a4.126 4.126 0 017.746 0 .75.75 0 01-.351.92 7.47 7.47 0 01-3.522.877 7.47 7.47 0 01-3.522-.877.75.75 0 01-.351-.92zM15 8.25a.75.75 0 000 1.5h3.75a.75.75 0 000-1.5H15zM14.25 12a.75.75 0 01.75-.75h3.75a.75.75 0 010 1.5H15a.75.75 0 01-.75-.75zm.75 2.25a.75.75 0 000 1.5h3.75a.75.75 0 000-1.5H15z"
 									clip-rule="evenodd"
 								/>
-							</svg> </button><button class="hover:bg-indigo-900 px-2 h-full"><svg
+							</svg> </button><button class="hover:bg-blue-900 px-2 h-full"><svg
 								xmlns="http://www.w3.org/2000/svg"
 								viewBox="0 0 24 24"
 								fill="currentColor"
@@ -41,7 +41,7 @@
 									d="M15.75 1.5a6.75 6.75 0 00-6.651 7.906c.067.39-.032.717-.221.906l-6.5 6.499a3 3 0 00-.878 2.121v2.818c0 .414.336.75.75.75H6a.75.75 0 00.75-.75v-1.5h1.5A.75.75 0 009 19.5V18h1.5a.75.75 0 00.53-.22l2.658-2.658c.19-.189.517-.288.906-.22A6.75 6.75 0 1015.75 1.5zm0 3a.75.75 0 000 1.5A2.25 2.25 0 0118 8.25a.75.75 0 001.5 0 3.75 3.75 0 00-3.75-3.75z"
 									clip-rule="evenodd"
 								/>
-							</svg> </button><button class="hover:bg-indigo-900 px-2 h-full"><svg
+							</svg> </button><button class="hover:bg-blue-900 px-2 h-full"><svg
 								xmlns="http://www.w3.org/2000/svg"
 								viewBox="0 0 24 24"
 								fill="currentColor"
